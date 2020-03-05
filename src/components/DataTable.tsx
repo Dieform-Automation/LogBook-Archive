6218d5e23cded53668ea29c13e648196d1ef0160
Hamza Basrai <hamza.basrai@gmail.com>
2020-03-04 19:16:24 -0500

    Added search to DataTable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Table } from 'semantic-ui-react';
-import { useTable, useSortBy } from 'react-table';
+import React, { useState, useMemo } from 'react';
+import { Table, Input, Dropdown, DropdownProps } from 'semantic-ui-react';
+import { useTable, useSortBy, useFilters } from 'react-table';
 
 type Props = {
   data: any;
@@ -13,41 +13,87 @@ const DataTable: React.FC<Props> = ({ columns, data }) => {
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow
-  } = useTable({ columns, data }, useSortBy);
+    prepareRow,
+    setFilter
+  } = useTable({ columns, data }, useFilters, useSortBy);
+
+  const filterOptions = useMemo(() => {
+    return headerGroups.flatMap(hg => {
+      return hg.headers.map((header, idx) => {
+        return { key: idx, text: header.Header, value: header.id };
+      });
+    });
+  }, [headerGroups]);
+
+  const [filterText, setFilterText] = useState<string>('');
+  const [filterField, setFilterField] = useState<string>(
+    filterOptions[0].value
+  );
+
+  const filterTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    if (typeof value === 'string') {
+      setFilterText(value);
+      setFilter(filterField, value);
+    }
+  };
+
+  const filterFieldChange = (
+    _: React.SyntheticEvent<HTMLElement, Event>,
+    data: DropdownProps
+  ) => {
+    if (typeof data.value === 'string') {
+      setFilterField(data.value);
+    }
+  };
 
   return (
-    <Table celled {...getTableProps()}>
-      <Table.Header>
-        {headerGroups.map(headerGroup => (
-          <Table.Row {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <Table.HeaderCell
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-              >
-                {column.render('Header')}
-              </Table.HeaderCell>
-            ))}
-          </Table.Row>
-        ))}
-      </Table.Header>
-      <Table.Body {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <Table.Row {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <Table.Cell {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </Table.Cell>
-                );
-              })}
+    <>
+      <Input
+        value={filterText}
+        onChange={filterTextChange}
+        placeholder="Search"
+        labelPosition="right"
+        fluid
+        label={
+          <Dropdown
+            selection
+            defaultValue={filterOptions[0].value}
+            options={filterOptions}
+            onChange={filterFieldChange}
+          />
+        }
+      />
+      <Table celled {...getTableProps()}>
+        <Table.Header>
+          {headerGroups.map(headerGroup => (
+            <Table.Row {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <Table.HeaderCell {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </Table.HeaderCell>
+              ))}
             </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
+          ))}
+        </Table.Header>
+        <Table.Body {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <Table.Row {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <Table.Cell {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </Table.Cell>
+                  );
+                })}
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </>
   );
 };
 
--- src/views/Customers.tsx
+++ src/views/Customers.tsx
@@ -11,22 +11,70 @@ export const Customers: React.FC = () => {
 
   const data: Array<Customer> = [
     {
-      name: 'Johns Shop',
-      address: {
-        city: 'Ottawa',
-        postalCode: 'K1S0C2',
-        province: 'ON',
-        street: '117 Lees Avenue'
-      },
+      name: 'Chinatown Chucksters',
+      city: 'Ottawa',
+      postalCode: 'L%V1C9',
+      province: 'ON',
+      street: '117 Lees Avenue',
       id: 1,
-      phone: '555-555-5555'
-    }
+      phone: '904-847-0942',
+      country: 'Canada',
+      pointOfContact: 'Stickster',
+      email: 'jellybelly@johnboy.com'
+    },
+    {
+      name: 'Grimlatar Steel',
+      city: 'Ottawa',
+      postalCode: 'K1S0C2',
+      province: 'ON',
+      street: '117 Lees Avenue',
+      id: 1,
+      phone: '123-873-3432',
+      country: 'Canada',
+      pointOfContact: 'Hickory',
+      email: 'gizmo@gadget.com'
+    },
+    {
+      name: 'Tesla',
+      city: 'Ottawa',
+      postalCode: 'K1S0C2',
+      province: 'ON',
+      street: '117 Lees Avenue',
+      id: 1,
+      phone: '232-645-1085',
+      country: 'Canada',
+      pointOfContact: 'Garbonza',
+      email: 'lamper.page@crest.com'
+    },
   ];
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Company',
+        accessor: 'name'
+      },
+      {
+        Header: 'Contact',
+        accessor: 'pointOfContact'
+      },
+
+      {
+        Header: 'Email',
+        accessor: 'email'
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone'
+      },
+    ],
+    []
+  );
 
   return (
     <PaddedContainer>
       <Header as="h1">Customers</Header>
-      <DataTable data={data}></DataTable>
+      <DataTable columns={columns} data={data}></DataTable>
     </PaddedContainer>
   );
 };
