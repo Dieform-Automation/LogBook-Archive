f1879b3ce3871f54a3f568a30e79f04422438eda
Hamza Basrai <hamza.basrai@gmail.com>
2020-03-10 18:14:37 -0400

    Added page structure to Parts component

--- src/views/Parts.tsx
+++ src/views/Parts.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from 'react';
+import Layout from '../components/Layout';
+import { Header, Button } from 'semantic-ui-react';
+import DataTable from '../components/DataTable';
+import PaddedContainer from '../styles/PaddedContainer';
 
 export const Parts: React.FC = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Customer',
+        accessor: 'customer_id'
+      },
+      {
+        Header: 'Part Name',
+        accessor: 'name'
+      },
+
+      {
+        Header: 'Part Number',
+        accessor: 'number'
+      }
+    ],
+    []
+  );
+
   return (
     <Layout>
-      
+      <PaddedContainer>
+        <Header as="h1">Parts</Header>
+        <DataTable columns={columns} data={[]} action={<Button />}></DataTable>
+      </PaddedContainer>
     </Layout>
-  )
-}
+  );
+};
